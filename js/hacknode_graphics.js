38016c25489b2dbc3bb4743a77e6cc4bd175b557
Cameron O'Rourke <cameron.orourke@oracle.com>
2016-12-31 20:54:59 -0800

    Merge new library updates and Oliver's game changes

--- js/hacknode_graphics.js
+++ js/hacknode_graphics.js
@@ -3,77 +3,68 @@ paper.install(window);
 window.nodes = [];
 window.lines = [];
 
+
 function Node(x, y, r, c) {
-    this.health = 100;
+
+
     this.name = "Node at " + x + "," + y + " with color " + c;
     this.connectedNodes = [];
-    
-    this.owned = false;
+
+    var _health = 100;
+    var owned = false;
     var w = 15; // border width
 
     var outerNode = new Path.Circle({
         center: new Point(x, y),
-        radius: r + w,
-        fillColor: c,
+        radius: r + w, // border width is 15px
         shadowColor: c,
         shadowBlur: 80,
         shadowOffset: new Point(0, 0)
     });
 
-    // Make the shadow
-    // If the node color is a shade of grey (no saturation) shadow color must be white
-    if (outerNode.fillColor.saturation == 0) {
-        outerNode.shadowColor = new Color(.7);
-    } else {
-        outerNode.shadowColor.brightness = 1; // Boost the shadow brightness to max
-        outerNode.shadowColor.saturation = 1; // Boost the shadow color saturation
-    }
-
     var innerNode = new Path.Circle({
         center: new Point(x, y),
         radius: r
     });
-    innerNode.fillColor = new Color(c);
-    innerNode.fillColor.brightness += .07;
-    innerNode.fillColor.saturation -= .1;
 
     var arcNode = createArc(x, y, r + w, c);
 
     // Make the damage indicator (text)
-    this.damageText = new PointText(new Point(x, y+10));
-    this.damageText.fontFamily = "Gill Sans";
-    this.damageText.fontSize = 24;
-    this.damageText.justification = 'center';
-    this.damageText.fillColor = 'white';
-    this.damageText.opacity = 0.5;
-    this.damageText.content = this.health;
+    var damageText = new PointText({
+        point: new Point(x, y + 10),
+        content: _health,
+        fillColor: 'white',
+        opacity: 0.5,
+        justification: 'center',
+        fontFamily: 'Gill Sans',
+        fontWeight: 'normal',
+        fontSize: 24
+    });
 
     // Make a transparent circle over the node as a consistent target for mouse events
-    this.mouseTarget = new Path.Circle({
+    var mouseTarget = new Path.Circle({
         center: new Point(x, y),
         radius: r + w,
         fillColor: new Color(1, 0.001)
     });
 
-    Group.call(this, [outerNode, innerNode, arcNode, this.damageText, this.mouseTarget]);
-
-    var r = this.bounds.width / 2; // radius
+    setBaseColor(c);
+    Group.call(this, [outerNode, innerNode, arcNode, damageText, mouseTarget]);
 
     // Make the select circle and hide it
-    this.selectNode = new Path.Circle({
+    var selectNode = new Path.Circle({
         center: new Point(this.position.x, this.position.y),
-        radius: r,
+        radius: r + w,
         strokeColor: 'white',
         strokeWidth: 3,
         shadowColor: 'white',
         shadowBlur: 40,
         shadowOffset: new Point(0, 0)
     });
-    this.selectNode.visible = false;
+    selectNode.visible = false;
 
-    // Make the target circle and hide it
-    var r = this.bounds.width / 2 + 10; // radius
-    this.targetCircle = new Path.Circle({
+    // Make the targeting circle and hide it
+    var targetCircle = new Path.Circle({
         center: new Point(this.position.x, this.position.y),
         radius: r,
         strokeColor: 'orange',
@@ -82,97 +73,96 @@ function Node(x, y, r, c) {
         shadowBlur: 40,
         shadowOffset: new Point(0, 0)
     });
-    this.targetCircle.visible = false;
+    targetCircle.visible = false;
 
+    // ACCESSORS
 
+    Object.defineProperty(this, "owned", {
+        get: function () {
+            return owned;
+        },
+        set: function (_bool) {
+            owned = _bool;
+            if(owned) { setBaseColor('#4286f4') };
+        }
+    });
+    Object.defineProperty(this, "selected", {
+        get: function () {
+            return selectNode.visible;
+        },
+        set: function (_selected) {
+            selectNode.visible = _selected;
+        }
+    });
+    Object.defineProperty(this, "targeted", {
+        get: function () {
+            return this.targetNode.visible;
+        },
+        set: function (_targeted) {
+            this.targetNode.visible = _targeted;
+        }
+    });
+    Object.defineProperty(this, "health", {
+        get: function () {
+            return _health;
+        },
+        set: function (amount) {
+            _health = amount;
+            damageText.content = _health;
+        }
+    });
+    Object.defineProperty(this, "baseColor", {
+        set: function (_color) {
+            setBaseColor(_color);
+        }
+    });
+
+    // PRIVATE FUNCTIONS
+
+    function setBaseColor(c) {
+        outerNode.fillColor = c;
+        // If the node color is a shade of grey (no saturation) shadow color must be white
+        if (outerNode.fillColor.saturation == 0) {
+            outerNode.shadowColor = new Color(.7);
+        } else {
+            outerNode.shadowColor = c;
+            outerNode.shadowColor.brightness = 1; // Boost the shadow brightness to max
+            outerNode.shadowColor.saturation = 1; // Boost the shadow color saturation
+        }
+        innerNode.fillColor = c;
+        innerNode.fillColor.brightness += .07;
+        innerNode.fillColor.saturation -= .1;
+        arcNode.fillColor = c;
+        arcNode.fillColor.alpha = 0.3;
+        arcNode.blendMode = 'multiply';
+    };
+
+    // Draws the half circle
+    function createArc(x, y, r) {
+        // Draw the outside curve of a half-circle
+        var start = new Point(x, y - r);
+        var through = new Point(x - r, y);
+        var to = new Point(x, y + r);
+        var arcNode = Path.Arc(start, through, to);
+        // Draw the flat line
+        arcNode.add(new Point(x, y));
+        arcNode.add(new Point(x, y - r));
+        arcNode.rotate(45, new Point(x, y));
+        return arcNode;
+    };
 }
 
 Node.prototype = Object.create(Group.prototype);
 Node.prototype.constructor = Group;
 
-Node.prototype.setOwned = function(_owned) {
-    this.owned = _owned;
-    if(this.owned) {
-        this.setBaseColor('#4286f4');         // Blue
-    }
-}
-Node.prototype.isOwned = function() {
-    return this.owned;
-}
 
-// Change this node's hue. Hue is from 0 to 360.
-Node.prototype.setHue = function(h) {
-    this.children[0].fillColor.hue = h;
-    this.children[0].shadowColor.hue = h;
-    this.children[1].fillColor.hue = h;
-    this.children[2].fillColor.hue = h;
-};
-
-// Set the node's base color.
-Node.prototype.setBaseColor = function(c) {
-    // TODO: DRY up this code
-    this.children[0].fillColor = c;
-    this.children[0].shadowColor = new Color(c);
-    this.children[0].shadowColor.brightness = 1; // Boost the shadow brightness to max
-    this.children[0].shadowColor.saturation = 1; // Boost the shadow color saturation
-    //this.children[1].fillColor = new Color(c).hue; // TODO: rafactor code with constructor
-    this.children[1].fillColor = new Color(c);
-    this.children[1].fillColor.brightness += .07;
-    this.children[1].fillColor.saturation -= .1;
-    //this.children[2].fillColor = new Color(c).hue; // TODO: compute color as in constructor
-    this.children[2].fillColor = new Color(c);
-    this.children[2].fillColor.alpha = 0.3;
-};
-
-Node.prototype.selectOn = function() {
-    this.selectNode.visible = true;
-};
-Node.prototype.selectOff = function() {
-    this.selectNode.visible = false;
-};
-Node.prototype.isSelected = function() {
-    return this.selectNode.visible;
-}
-Node.prototype.targetOn = function() {
-    this.targetCircle.visible = true;
-};
-Node.prototype.targetOff = function() {
-    this.targetCircle.visible = false;
-};
-Node.prototype.isTargeted = function() {
-    return this.targetCircle.visible;
-}
-Node.prototype.setHealth = function(amount) {
-    this.health = amount;
-    this.damageText.content = this.health;
-}
-
-
+// TODO: this is no longer valid
 // Does this node's connections include the node with id number n?
 // usage: node.isConnected(2);
-Node.prototype.isConnected = function(n) {
+Node.prototype.isConnected = function (n) {
     return this.connections.includes(n);
 }
 
 // ### Standalone functions
 
-// Connect two nodes in nodes[] with the index numbers n1 and n2
 
-
-
-// Draws the half circle
-function createArc(x, y, r, c) {
-    // Draw the outside curve of a half-circle
-    var start = new Point(x, y - r);
-    var through = new Point(x - r, y);
-    var to = new Point(x, y + r);
-    var arcNode = Path.Arc(start, through, to);
-    // Draw the flat line
-    arcNode.add(new Point(x, y));
-    arcNode.add(new Point(x, y - r));
-    arcNode.fillColor = c;
-    arcNode.fillColor.alpha = 0.3;
-    arcNode.blendMode = 'multiply';
-    arcNode.rotate(45, new Point(x, y));
-    return arcNode;
-}
